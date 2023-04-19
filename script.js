7ab093c6dda9747b1418ba2e913a83481813a61b
maxerton <maxerton13.k@gmail.com>
2023-04-19 20:07:03 +0300

    update

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ class Films {
   #data;
   #key = '870c6e64';
 
-  constructor(form, films) {
+  constructor(form, films, fd) {
     this.form = form;
     this.films = films;
+    this.filmDetailes = fd;
     this.titleSearch = this.form.querySelector('input[data-name=title]');
     this.selectSearch = this.form.querySelector('select[data-name=type]');
     this.filmsContent = this.films.querySelector('.films__content');
@@ -51,9 +52,26 @@ class Films {
         type: this.selectSearch.value,
         page: page
       },
-      success: function(request) {
-        console.dir(this);
-        _this.successReq(request);
+      success: function (response) {
+        _this.successReq(response);
+      }
+    });
+  }
+
+
+  requestFilm(id) {
+    console.log(1);
+    const _this = this;
+    $.ajax({
+      type: "get",
+      url: 'http://www.omdbapi.com/',
+      data: {
+        apikey: this.#key,
+        i: id,
+        plot: 'full'
+      },
+      success: function (response) {
+        _this.renderInfo(response)
       }
     });
   }
@@ -68,17 +86,61 @@ class Films {
     }
   }
 
+  renderInfo(data) {
+    let htm = `
+    <h4>Film info</h4>
+    <div class="film-details__content">
+      <div class="img-wrapper">
+        <img src="${data.Poster}">
+      </div>
+      <div class="film-data">
+        <div class="data-group">
+          <div class="title">Title:</div>
+          <div class="content">${data.Title}</div>
+        </div>
+        <div class="data-group">
+          <div class="title">Released:</div>
+          <div class="content">${data.Released}</div>
+        </div>
+        <div class="data-group">
+          <div class="title">Genre:</div>
+          <div class="content">${data.Genre}</div>
+        </div>
+        <div class="data-group">
+          <div class="title">Country:</div>
+          <div class="content">${data.Country}</div>
+        </div>
+        <div class="data-group">
+          <div class="title">Director:</div>
+          <div class="content">${data.Director}</div>
+        </div>
+        <div class="data-group">
+          <div class="title">Writer:</div>
+          <div class="content">${data.Writer}</div>
+        </div>
+        <div class="data-group">
+          <div class="title">Actors:</div>
+          <div class="content">${data.Actors}</div>
+        </div>
+        <div class="data-group">
+          <div class="title">Awards:</div>
+          <div class="content">${data.Awards}</div>
+        </div>
+      </div>
+    </div>
+    `;
+    this.filmDetailes.innerHTML = htm;
+  }
+
   refreshPaginator(counts) {
     this.paginator.querySelectorAll('[data-func="num"]').forEach(e => e.remove());
     console.log(counts);
     console.log((Math.ceil(counts / 10)));
     for (let i = 1; i < (Math.ceil(counts / 10)); i++) {
-      console.log(i);
-      this.paginator.querySelector('li:last-child').insertAdjacentHTML('beforebegin', `<li data-func="num">${i}</li>`);
+      this.paginator.querySelector('li:last-child').insertAdjacentHTML('beforebegin', `<li data-func="num" ${this.currentPage === i ? 'class="active"' : ''}>${i}</li>`);
     }
   }
 
-  //{"Search":[{"Title":"Ocean Waves","Year":"1993","imdbID":"tt0108432","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BNDkzODhjMTktOTkwZS00MGQ3LTg4MGUtMDA5NWI2Y2ZiMzhjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg"},{"Title":"The Deep End of the Ocean","Year":"1999","imdbID":"tt0120646","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BODVhNDEyYzktMmU2Ni00NzlkLTg0NWQtYmEzYTg4MDliOWQ3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"},{"Title":"A Plastic Ocean","Year":"2016","imdbID":"tt5203824","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTk5MTU0MTA2OF5BMl5BanBnXkFtZTgwMzQ3MjQ5MDI@._V1_SX300.jpg"},{"Title":"Ocean Heaven","Year":"2010","imdbID":"tt1498858","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMjMyNDk5ODgxM15BMl5BanBnXkFtZTgwODQ4NjI2MDE@._V1_SX300.jpg"},{"Title":"711 Ocean Drive","Year":"1950","imdbID":"tt0042176","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMzE0OWEyNjAtM2QyNS00MDk5LWI1ZGQtNjU0NjVmZjg2OTRmXkEyXkFqcGdeQXVyNTk1MTk0MDI@._V1_SX300.jpg"},{"Title":"Planet Ocean","Year":"2012","imdbID":"tt2240784","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BOGFkZjdjYjEtN2NmOS00NWJkLWJmZWItM2Q2NTBmY2UzYWRjXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"},{"Title":"Monster from the Ocean Floor","Year":"1954","imdbID":"tt0047244","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BN2U3YmIxMTMtOTNjMy00MGZlLWJhMGEtNzM3NzM5ZWFiZTVlXkEyXkFqcGdeQXVyNjQzNDI3NzY@._V1_SX300.jpg"},{"Title":"Ocean Boy","Year":"2022","imdbID":"tt11898882","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BY2I1ZTZlYmQtZjZiYS00ZWIzLWJmMjEtMWY2ZmExZGE0N2NhXkEyXkFqcGdeQXVyNTEwNjM1MjE@._V1_SX300.jpg"},{"Title":"The Ocean Waif","Year":"1916","imdbID":"tt0004413","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMzYwNTQ5YzYtNjA5Zi00MzI4LWI2NDAtYjU1NDYyNThlNmYyXkEyXkFqcGdeQXVyNjA5MTAzODY@._V1_SX300.jpg"},{"Title":"Ocean of Pearls","Year":"2008","imdbID":"tt0871438","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTQ2ODYwNjUwMF5BMl5BanBnXkFtZTcwOTc5OTc0Mg@@._V1_SX300.jpg"}],"totalResults":"591","Response":"True"}
 
   formWorker(ev) {
     if (ev.target.matches('[type=submit]')) {
@@ -88,15 +150,37 @@ class Films {
     }
   }
 
+  showInfo(ev) {
+    if (ev.target.matches('button[data-id]')) {
+      this.requestFilm(ev.target.dataset.id);
+    }
+  }
+  
+  paginatorService(ev) {
+    if (ev.target.matches('li[data-func]')) {
+      if (ev.target.dataset.func === 'num') {
+        this.currentPage = +ev.target.textContent;
+      } else if (ev.target.dataset.func === 'prev') {
+        if (this.currentPage > 1) {
+          this.currentPage--;
+        }
+      } else if (ev.target.dataset.func === 'next') {
+        if (this.currentPage < +this.paginator.querySelector('li:nth-last-child(2)').textContent) {
+          this.currentPage++;
+        }
+      }
+      this.requested(this.currentPage);
+    }
+  }
+
   init() {
     this.form.addEventListener('click', this.formWorker.bind(this));
-    // this.form.addEventListener('submit', this.formWorker.bind(this));
-    // console.log('123')
-    console.dir(this.films.querySelector('.films__content'));
+    this.films.querySelector('.films__content').addEventListener('click', this.showInfo.bind(this));
+    this.paginator.addEventListener('click', this.paginatorService.bind(this));
   }
 }
 $(document).ready(function () {
-  const films = new Films(document.querySelector('form.form'), document.querySelector('.films'));
+  const films = new Films(document.querySelector('form.form'), document.querySelector('.films'), document.querySelector('.film-details'));
   films.init();
 })
 
